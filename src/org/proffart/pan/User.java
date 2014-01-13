35d838bcfc8f3a4dea9b6b887816519b88fff781
Ashot <mnatsakanyan.ashot@gmail.com>
2014-01-13 23:58:32 +0400

    commit from test

--- src/org/proffart/pan/User.java
+++ src/org/proffart/pan/User.java
@@ -1,6 +1,8 @@
 package org.proffart.pan;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,7 +26,8 @@ public class User extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.print("Test Servlet is work !");
 	}
 
 	/**
