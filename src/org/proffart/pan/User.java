30d7c52fa79d1cab050003404472c0ba3d43b411
Aram89 <aramkirakosyan89@gmail.com>
2014-01-13 12:37:25 -0800

    changes in User.java

--- src/org/proffart/pan/User.java
+++ src/org/proffart/pan/User.java
@@ -27,7 +27,7 @@ public class User extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		out.print("Test Servlet is work !");
+		out.print("Barev Axper Jan !");
 	}
 
 	/**
