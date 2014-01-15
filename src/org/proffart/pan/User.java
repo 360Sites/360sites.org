391272110c61c3a068b90047d08df8952d015697
Karen <karen9331@mail.ru>
2014-01-16 00:10:53 +0400

    Added log4j
    Note!
    For using log4j change log = E:/LOG to Your path in
    360sites.org\properties\log4j.properties
    
    1.Added log4j simple log
    2.Chenged project lib folder

--- src/org/proffart/pan/User.java
+++ src/org/proffart/pan/User.java
@@ -8,11 +8,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
 /**
  * Servlet implementation class User
  */
 public class User extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	static Logger LOG = Logger.getLogger(User.class);
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -28,6 +31,7 @@ public class User extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 	    out.print("Barev Axper Jan !");
+	    LOG.info("Test log4j");
 	}
 
 	/**
