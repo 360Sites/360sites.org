6864ded62c658cbe77b43b07ef356d8b0ec4c6fe
Ashot <mnatsakanyan.ashot@gmail.com>
2014-01-18 01:34:56 +0400

    Create User class , and login method

--- src/org/proffart/pan/User.java
+++ src/org/proffart/pan/User.java
@@ -1,54 +1,70 @@
 package org.proffart.pan;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.HashMap;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 
-/**
- * Servlet implementation class User
- */
-public class User extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	static Logger LOG = Logger.getLogger(User.class);
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public User() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-	    out.println("Barev Axper Jan !");
-	    LOG.info("Test log4j");
-	    try{
-		    DbManager db = DbManager.getInstance();
-		    String sql = "SELECT * FROM `user` WHERE `user_name` = 'admin'";
-		    HashMap<String, String> res = db.getRow(sql);
-		    out.println("Barev "+res.get("first_name")+" Jan !");
-	    }catch(Exception e){
-	    	e.printStackTrace();
-	    }
-	    
+public class User {
+	
+	static HttpSession session;
+	static Logger LOG = Logger.getLogger(DbManager.class);
+	
+	public static void setSession(HttpSession setSession) {
+		session = setSession;
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	
+	public static boolean isLogined() {
+		boolean isLogined = false;
+		if( !session.isNew() ) {
+			isLogined = (Boolean) session.getAttribute("isLogined");
+		}
+		return isLogined;
 	}
-
+	
+	public static boolean login(String userName, String password) {
+		boolean isLogined = false;
+		try {
+			DbManager db = DbManager.getInstance();
+			Connection connection  = DbManager.getConnection();
+			String sql = "SELECT *, MD5(?) AS external_password FROM `user` WHERE `user_name` = ?";
+			PreparedStatement pstmt = connection.prepareStatement(sql);
+			pstmt.setString(1, password);
+			pstmt.setString(2, userName);
+			HashMap<String, String> userInfo = db.getRow(pstmt);
+			if( userInfo.get("id") != null ) {
+				if( userInfo.get("password").equals(userInfo.get("external_password")) ) {
+					boolean isValidUser = (userInfo.get("is_valid_user").equals("1"));
+					if( isValidUser ) {
+						isLogined = true;
+						session.setAttribute("user_name", userInfo.get("user_name"));
+						session.setAttribute("user_id", userInfo.get("id"));
+						session.setAttribute("user_type_id", userInfo.get("user_type_id"));
+						session.setAttribute("first_name", userInfo.get("first_name"));
+						session.setAttribute("email", userInfo.get("email"));
+						session.setAttribute("phon_number", userInfo.get("phon_number"));
+					}else{
+						LOG.error("user is invalid");
+					}
+				}else{
+					LOG.error("incorrect password");
+				}
+			}else{
+				LOG.error("userName '"+userName+"' cannot exist");
+			}
+		} catch(SQLException e) {
+			LOG.error("Error DB: "+e.getMessage());
+			e.printStackTrace();
+		} catch(Exception e) {
+			LOG.error("Error DB: "+e.getMessage());
+			e.printStackTrace();
+		}
+		session.setAttribute("isLogined", isLogined);
+		return isLogined;
+	}
 }
