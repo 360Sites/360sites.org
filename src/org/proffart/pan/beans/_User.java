f1589d13bb1d8fa22df6ce2ef1a95f39906ad1e2
Ashot <mnatsakanyan.ashot@gmail.com>
2014-01-28 02:12:58 +0400

    extends _Base

--- src/org/proffart/pan/beans/_User.java
+++ src/org/proffart/pan/beans/_User.java
@@ -2,19 +2,15 @@ package org.proffart.pan.beans;
 
 import org.proffart.pan.User;
 
-public class _User {
-	private User instance;
+public class _User extends _Base {
 	
 	/*-arguments-*/
 	private String userName;
 	private String password;
 	/*-end-arguments-*/
 	
-	public _User() {
-		instance = new User();
-	}
-	
 	public boolean login() throws Exception {
+		User instance = new User(request);
 		return instance.login(userName, password);
 	}
 	
