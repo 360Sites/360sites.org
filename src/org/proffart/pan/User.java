facf7506078d935509de671929ce61e9ddfd8071
Ashot <mnatsakanyan.ashot@gmail.com>
2014-01-27 16:32:57 +0400

    modified

--- src/org/proffart/pan/User.java
+++ src/org/proffart/pan/User.java
@@ -2,7 +2,6 @@ package org.proffart.pan;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.HashMap;
 
 import javax.servlet.http.HttpSession;
@@ -15,12 +14,6 @@ import org.apache.log4j.Logger;
  *
  */
 public class User {
-	/*-arguments-*/
-	private String userName;
-	private String password;
-	/*-end-arguments-*/
-	
-	
 	
 	private static HttpSession session;
 	static Logger LOG = Logger.getLogger(DbManager.class);
@@ -47,53 +40,44 @@ public class User {
 		}
 		return lang;
 	}
-	public boolean login() {
+	public boolean login(String userName, String password) throws Exception {
 		boolean isLogined = false;
-		try {
-			DbManager db = DbManager.getInstance();
-			Connection connection  = DbManager.getConnection();
-			
-			//generating sql query
-			String sql = "SELECT *, MD5(?) AS external_password FROM `user` WHERE `user_name` = ?";
-			PreparedStatement pstmt = connection.prepareStatement(sql);
-			pstmt.setString(1, password);
-			pstmt.setString(2, userName);
-			HashMap<String, String> userInfo = db.getRow(pstmt);
-			if( userInfo.get("id") != null ) { //ete tox ka uremn etenc user ka u id uni
-				if( userInfo.get("password").equals(userInfo.get("external_password")) ) { //hamematum enq bazay u ekac passwordneri md5 hasery
-					boolean isValidUser = (userInfo.get("is_valid_user").equals("1"));
-					if( isValidUser ) { //stugum enq tvyal usery akti e te voch ete voch login cheq anum
-						isLogined = true;
-						//saving session information
-						session.setAttribute("user_name", userInfo.get("user_name"));
-						session.setAttribute("user_id", userInfo.get("id"));
-						session.setAttribute("user_type_id", userInfo.get("user_type_id"));
-						session.setAttribute("first_name", userInfo.get("first_name"));
-						session.setAttribute("email", userInfo.get("email"));
-						session.setAttribute("phone_number", userInfo.get("phone_number"));
-						session.setAttribute("lang", "en"); //berel bazayic
-					}else{
-						Notification.error(1000);
-						//LOG.error("user is invalid");
-					}
+
+		DbManager db = DbManager.getInstance();
+		Connection connection  = DbManager.getConnection();
+		
+		//generating sql query
+		String sql = "SELECT *, MD5(?) AS external_password FROM `user` WHERE `user_name` = ?";
+		PreparedStatement pstmt = connection.prepareStatement(sql);
+		pstmt.setString(1, password);
+		pstmt.setString(2, userName);
+		HashMap<String, String> userInfo = db.getRow(pstmt);
+		if( userInfo.get("id") != null ) { //ete tox ka uremn etenc user ka u id uni
+			if( userInfo.get("password").equals(userInfo.get("external_password")) ) { //hamematum enq bazay u ekac passwordneri md5 hasery
+				boolean isValidUser = (userInfo.get("is_valid_user").equals("1"));
+				if( isValidUser ) { //stugum enq tvyal usery akti e te voch ete voch login cheq anum
+					isLogined = true;
+					//saving session information
+					session.setAttribute("user_name", userInfo.get("user_name"));
+					session.setAttribute("user_id", userInfo.get("id"));
+					session.setAttribute("user_type_id", userInfo.get("user_type_id"));
+					session.setAttribute("first_name", userInfo.get("first_name"));
+					session.setAttribute("email", userInfo.get("email"));
+					session.setAttribute("phone_number", userInfo.get("phone_number"));
+					session.setAttribute("lang", "en"); //berel bazayic
 				}else{
-					Notification.error(1001);
-					//LOG.error("incorrect password");
+					Notification.error(1000);
+					//LOG.error("user is invalid");
 				}
 			}else{
-				Notification.textSprintfArgs = new Object[1];
-				Notification.textSprintfArgs[0] = userName;
-				Notification.error(1002);
-				//LOG.error("userName '" +userName+ "' does not exist");
+				Notification.error(1001);
+				//LOG.error("incorrect password");
 			}
-		} catch(SQLException e) {
-			Notification.error(e.getMessage());
-			//LOG.error("Error DB: " + e.getMessage());
-			//e.printStackTrace();
-		} catch(Exception e) {
-			Notification.error(e.getMessage());
-			//LOG.error("Error DB: " + e.getMessage());
-			//e.printStackTrace();
+		}else{
+			Notification.textSprintfArgs = new Object[1];
+			Notification.textSprintfArgs[0] = userName;
+			Notification.error(1002);
+			//LOG.error("userName '" +userName+ "' does not exist");
 		}
 		session.setAttribute("isLogined", isLogined);
 		return isLogined;
