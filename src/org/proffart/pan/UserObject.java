28a258ba4be7a729e362320c7039a3b6f8c7f313
Ashot <Ashot@pc>
2014-02-07 23:16:30 +0400

    code review

--- src/org/proffart/pan/UserObject.java
+++ src/org/proffart/pan/UserObject.java
@@ -5,8 +5,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
@@ -26,20 +24,17 @@ public class UserObject extends Base{
 	 * @throws IOException 
 	 * @throws ClassNotFoundException 
 	 */
-	public static int createObject (HttpServletRequest r, HashMap <String, Object> fields) throws ClassNotFoundException, IOException, SQLException
+	public int createObject ( HashMap <String, Object> fields) throws ClassNotFoundException, IOException, SQLException
 	{
 		HashMap <String, Object> rows = new HashMap<>();
 		rows.put("id", fields.get("id"));
-		rows.put("user_id", fields.get(User.getId(r)));
+		rows.put("user_id", fields.get(User.getId(request)));
 		rows.put("name", fields.get("name"));
 		rows.put("description", fields.get("descripton"));
 		rows.put("location", fields.get("location"));
-		rows.put("delete_day", null);
-		//adding status field for deleted data, by default "NO"
-		rows.put("isdeleted", false);
 		DbManager db = DbManager.getInstance();
 		int id = db.insert("object", rows);
-		return id;		
+		return id;
 	}
 	
 	/**
@@ -50,11 +45,10 @@ public class UserObject extends Base{
 	 * @throws IOException
 	 * @throws SQLException
 	 */
-	public static void deleteObject (HttpServletRequest r, int id) throws ClassNotFoundException, IOException, SQLException
+	public void deleteObject (int id) throws ClassNotFoundException, IOException, SQLException
 	{
-		String query = "UPDATE object SET isdeleted = true AND delete_day = NOW() WHERE id = id AND user_id = 'User.getId(r)'";
-		DbManager instance = DbManager.getInstance();
-		Connection con = instance.getConnection();
+		String query = "UPDATE object SET isdeleted = true AND delete_day = NOW() WHERE id = "+id+" AND user_id = "+User.getId(request);
+		Connection con = DbManager.getConnection();
 		PreparedStatement pstmt = con.prepareStatement(query);
 		ResultSet rst = pstmt.executeQuery(query);				
 	}
