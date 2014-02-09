20532c476e19c7a202f583bfdf53bb4138470df3
Ashot <mnatsakanyan.ashot@gmail.com>
2014-02-09 23:51:48 +0400

    add "file" table create

--- sql/create.sql
+++ sql/create.sql
@@ -61,4 +61,27 @@ CREATE TABLE `notification`(
 INSERT INTO `notification` (`id`, `code`, `type`, `text_en`, `title_en`) VALUES 
 ('1','1000','E','user is invalid',''),
 ('2','1001','E','incorrect password',''),
-('3','1002','E','userName \'%s\' does not exist','');
+('3','1002','E','userName "%s" does not exist','');
+
+CREATE TABLE `file`(  
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) NOT NULL,
+  `full_path` CHAR(255) NOT NULL,
+  `url_path` CHAR(255) NOT NULL,
+  `name` CHAR(100) NOT NULL,
+  `mime_type` CHAR(50) NOT NULL,
+  `creation_date` DATETIME,
+  `seize` BIGINT,
+  `del_status` BOOLEAN DEFAULT FALSE,
+  `del_date` DATETIME,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB CHARSET=utf8 COLLATE=utf8_bin;
+
+
+
+
+
+
+
+
+
