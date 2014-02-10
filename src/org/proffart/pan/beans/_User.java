07e55955689094b99edd3ada5c820c7a7f2427bb
Ashot <mnatsakanyan.ashot@gmail.com>
2014-02-10 22:04:36 +0400

    modified

--- src/org/proffart/pan/beans/_User.java
+++ src/org/proffart/pan/beans/_User.java
@@ -3,17 +3,20 @@ package org.proffart.pan.beans;
 import org.proffart.pan.User;
 
 public class _User extends _Base {
-	
+	private User instance;
 	/*-arguments-*/
 	private String userName;
 	private String password;
 	private String language;
 	/*-end-arguments-*/
-	
+	public void createInstance() {
+		instance = new User(request);
+	}
+
 	public boolean login() throws Exception {
-		User instance = new User(request);
 		return instance.login(userName, password);
 	}
+
 	public String getLanguage()
 	{
 		return language;
