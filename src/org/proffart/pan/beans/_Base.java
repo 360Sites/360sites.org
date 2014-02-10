91e78489408b1dcbc21c60fc2306a6a3273f472b
Ashot <mnatsakanyan.ashot@gmail.com>
2014-02-10 21:52:03 +0400

    add createInstance

--- src/org/proffart/pan/beans/_Base.java
+++ src/org/proffart/pan/beans/_Base.java
@@ -4,8 +4,11 @@ import javax.servlet.http.HttpServletRequest;
 
 public class _Base {
 	protected HttpServletRequest request;
+	protected Object instance;
 	
 	public void setRequest(HttpServletRequest r) {
 		request = r;
 	}
+	
+	public void createInstance() {}
 }
